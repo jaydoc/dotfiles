0f4f198d16df22ced347f21084b9e0581fbaa024
serdotlinecho <serdotlinecho@gmail.com>
2014-04-16 11:53:28 +0800

    added titlebar icons

--- config/awesome/themes/holo/theme.lua
+++ config/awesome/themes/holo/theme.lua
@@ -25,6 +25,8 @@ theme.border_marked                 = "#6D9CBE"
 theme.taglist_fg_focus              = "#FFFFFF"
 theme.taglist_bg_focus              = "#303030"
 theme.tasklist_fg_focus             = theme.fg_normal
+theme.titlebar_bg_normal            = theme.bg_normal
+theme.titlebar_bg_focus             = theme.bg_normal
 theme.menu_width                    = "110"
 theme.menu_border_width             = "0"
 theme.menu_fg_normal                = "#C2C2C2"
@@ -68,6 +70,29 @@ theme.layout_tilebottom             = theme.confdir .. "/icons/tilebottom.png"
 theme.layout_tiletop                = theme.confdir .. "/icons/tiletop.png"
 theme.layout_fairv                  = theme.confdir .. "/icons/fairv.png"
 theme.layout_fairh                  = theme.confdir .. "/icons/fairh.png"
+
+theme.titlebar_close_button_focus               = theme.confdir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = theme.confdir .. "/icons/titlebar/close_normal.png"
+
+theme.titlebar_ontop_button_focus_active        = theme.confdir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active       = theme.confdir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.confdir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.confdir .. "/icons/titlebar/ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active       = theme.confdir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active      = theme.confdir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.confdir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.confdir .. "/icons/titlebar/sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active     = theme.confdir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active    = theme.confdir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive   = theme.confdir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive  = theme.confdir .. "/icons/titlebar/floating_normal_inactive.png"
+
+theme.titlebar_maximized_button_focus_active    = theme.confdir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.confdir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.confdir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.confdir .. "/icons/titlebar/maximized_normal_inactive.png"
 
 -- lain related
 theme.useless_gap_width             = 5
